2905c4a640d9ec73c488ec60de0129c3d7357059
Kun-Lin Ho <kun-linho@Kun-Lins-MacBook-Pro.local>
2020-02-28 11:48:35 -0500

    modify find VCF

--- find_vcf.py
+++ find_vcf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 25 15:52:10 2019
@@ -5,12 +6,23 @@ Created on Mon Nov 25 15:52:10 2019
 @author: abc73_000
 """
 
-with open('C:/Users/abc73_000/Desktop/retro_gene_list_information.txt','r') as f:
+"""
+first argument is retro_genefile information path contains gene name and gene location
+second argument is the VCF file path
+
+"""
+import sys
+
+retro_gene_info = sys.argv[1]
+#
+vcf_info = sys.argv[2]
+#
+with open(retro_gene_info,'r') as f:
     candidate_file = f.read()
     candidate_list =candidate_file.split('\n')[1:-1]
 
-with open('C:/Users/abc73_000/Desktop/CMT-2_rg_added_sorted_dedupped_removed.MuTect.vcf-PASS','r') as f:
-    file = f.read()
+with open(vcf_info,'r') as f1:
+    file = f1.read()
     vcf_file = file.split('\n')[:-1]
 
 cnt = 0
@@ -22,11 +34,8 @@ for i in range(len(vcf_file)):
         if chr_number == other_chr :
             first_pos = int(candidate_list[j].split('\t')[2])
             last_pos = int(candidate_list[j].split('\t')[3])
-            if (first_pos<loc< last_pos) :
-               print(chr_number)
-               print(cnt)
-           #if (int(candidate_list[0].split('\t')[2])<=loc<=int(candidate_list[0].split('\t')[3])):
-           #    cnt+=1
+            if (first_pos<=loc<= last_pos) :
+                cnt+=1
        
 
 print(cnt)
